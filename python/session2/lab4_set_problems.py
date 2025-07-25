11005b655199cfb391cc76b2cff2dc904e88bfab
Omar Elmasri <omarmasri@duck.com>
2025-07-25 03:21:20 +0300

    sol: sol set problems

--- python/session2/lab4_set_problems.py
+++ python/session2/lab4_set_problems.py
@@ -11,7 +11,11 @@ def set_operations(set1: set, set2: set):
     Returns:
         dict: Dictionary with union, intersection, difference
     """
-    # Write your solution here
+    return {
+        "union": set1.union(set2),
+        "intersection": set1.intersection(set2),
+        "difference": set1.difference(set2),
+    }
 
 
 def find_unique_elements(list1, list2):
@@ -24,7 +28,7 @@ def find_unique_elements(list1, list2):
     Returns:
         tuple: (unique_to_list1, unique_to_list2)
     """
-    # Write your solution here
+    return (set(list1).difference(set(list2)), set(list2).difference(set(list1)))
 
 
 def remove_vowels_set(text):
@@ -36,7 +40,10 @@ def remove_vowels_set(text):
     Returns:
         str: Text with vowels removed
     """
-    # Write your solution here
+    vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+    for ch in vowels:
+        text.replace(ch, "")
+    return text
 
 
 if __name__ == "__main__":
